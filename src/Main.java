5121da5642a91c6d8282edfc161fe7bda946df95
AbdulazizovElmar1 <emaboss19@gmail.com>
2024-02-11 21:16:56 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            int age = generateRandomAge();
+            int temperature = generateRandomTemperature();
+            String result = permission(age, temperature);
+            System.out.println("Возраст: " + age + ", Температура: " + temperature + " - " + result);
+        }
+    }
+
+    public static String permission(int age, int temperature) {
+        if ((age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) ||
+                (age < 20 && temperature >= 0 && temperature <= 28) ||
+                (age > 45 && temperature >= -10 && temperature <= 25)) {
+            return "Можно идти гулять";
+        } else {
+            return "Оставайтесь дома";
+        }
+    }
+
+    public static int generateRandomAge() {
+        Random random = new Random();
+        return random.nextInt(71);
+    }
+
+    public static int generateRandomTemperature() {
+        Random random = new Random();
+        return random.nextInt(51) - 20;
+    }
+}
